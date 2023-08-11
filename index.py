850df9cea172e2930cab5a81e7f8a000ed8973c3
Wesley Benício <wesleybenicio4@gmail.com>
2023-08-11 09:59:15 -0300

    final Commit

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from pytube import YouTube
 import requests
+import os,sys
 from bs4 import BeautifulSoup
+import shutil
 
 # YouTube('https://youtu.be/2lAe1cqCOXo').streams.first().download()
 # yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
@@ -13,27 +15,49 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 
-browser = webdriver.Chrome()
-browser.get('https://music.youtube.com/watch?v=fgXXwUsr2Ng&list=RDAMVMfgXXwUsr2Ng')
-action = ActionChains(browser)
+from selenium.webdriver.chrome.options import Options
 
-elem = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, "contents"))
-    )
+chrome_options = Options()
+chrome_options.add_argument('--headless')
 
-# list_elems = browser.find_elements(By.TAG_NAME,"ytmusic-player-queue-item")
-# list_elems = [x for x in list_elems if x.text != '']
-# for elem in range(len(list_elems)):
-#     list_elems = browser.find_elements(By.TAG_NAME,"ytmusic-player-queue-item")
-#     list_elems = [x for x in list_elems if x.text != '']
-#     list_elems[elem].find_element(By.ID,"play-button").click()
-#     print(browser.current_url)
-while True:
-    yt = YouTube(browser.current_url.split("&")[0].replace("music.",""))
-    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download("./musics")
-    body = browser.find_element(By.TAG_NAME,'body')
-    body.send_keys(Keys.SHIFT +  "n")
-    elem = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.ID, "contents"))
-    )
+browser = webdriver.Chrome(chrome_options=chrome_options)
+def main(first_url = None, limit=200):
     
+    browser.get(first_url)
+    action = ActionChains(browser)
+
+    _ = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, "contents"))
+        )
+
+    # list_elems = browser.find_elements(By.TAG_NAME,"ytmusic-player-queue-item")
+    # list_elems = [x for x in list_elems if x.text != '']
+    # for elem in range(len(list_elems)):
+    #     list_elems = browser.find_elements(By.TAG_NAME,"ytmusic-player-queue-item")
+    #     list_elems = [x for x in list_elems if x.text != '']
+    #     list_elems[elem].find_element(By.ID,"play-button").click()
+    #     print(browser.current_url)
+    body = browser.find_element(By.TAG_NAME,'body')
+    body.send_keys("m")
+    yt = YouTube(browser.current_url.split("&")[0].replace("music.",""))
+    paste_name=yt.title
+    while True:
+        yt = YouTube(browser.current_url.split("&")[0].replace("music.",""))
+        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').first().download("./musics")
+        try:
+            os.mkdir("./musics/"+paste_name)
+        except Exception as e:
+            pass
+        if(yt.title+".mp4" in os.listdir("./musics")):
+            
+            shutil.move("./musics/"+yt.title+".mp4","./musics/"+paste_name+"/"+yt.title+".mp4")
+        body.send_keys(Keys.SHIFT +  "n")
+        elem = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, "contents"))
+        )
+        print("quantidade de musicas: ",len(os.listdir("./musics/"+paste_name)))
+        if len(os.listdir("./musics/"+paste_name)) >= limit:
+            break
+
+if "__main__" == __name__:
+    main(first_url=sys.argv[1],limit = int(sys.argv[2]))
